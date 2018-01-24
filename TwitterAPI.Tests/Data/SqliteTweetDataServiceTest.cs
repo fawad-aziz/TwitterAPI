bbbf616affdc8952c58607bebdfd6ed39359c24b
Fawad Aziz <fawadaziz@live.com>
2018-01-24 11:26:48 -0500

    Async Queryable support and unit test

--- TwitterAPI.Tests/Data/SqliteTweetDataServiceTest.cs
+++ TwitterAPI.Tests/Data/SqliteTweetDataServiceTest.cs
@@ -2,6 +2,7 @@
 using Moq;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
 using System.Linq;
 using System.Threading.Tasks;
 using TwitterAPI.Data;
@@ -15,8 +16,8 @@ namespace TwitterAPI.Tests.Data
     {
         List<Tweet> setupList = new List<Tweet>()
         {
-            new Tweet() { UserName = "salesforce", CreatedAt = "Sun Jan 21 21:30:07 +0000 2018", ProfileImageUrl = "", RetweetCount = 1, ScreenName = "salesforce", Text = "First in list" },
-            new Tweet() { UserName = "salesforce", CreatedAt = "Sun Jan 21 21:30:07 +0000 2018", ProfileImageUrl = "", RetweetCount = 2, ScreenName = "salesforce", Text = "Second in list" }
+            new Tweet() { UserName = "salesforce", CreatedAt = "Sun Jan 21 21:30:07 +0000 2018", ProfileImageUrl = "", RetweetCount = 1, ScreenName = "salesforce", Text = "First in list", IsCurrent = true },
+            new Tweet() { UserName = "salesforce", CreatedAt = "Sun Jan 21 21:30:07 +0000 2018", ProfileImageUrl = "", RetweetCount = 2, ScreenName = "salesforce", Text = "Second in list", IsCurrent = false }
         };
 
         private static DbSet<T> GetQueryableMockDbSet<T>(params T[] sourceList) where T : class
@@ -33,7 +34,7 @@ namespace TwitterAPI.Tests.Data
         }
 
         [TestMethod]
-        public async Task GetCurrentTweetsTest()
+        public async Task UpdateTweetsTest()
         {
             var mockSet = new Mock<DbSet<Tweet>>();
 
@@ -44,6 +45,30 @@ namespace TwitterAPI.Tests.Data
             await service.UpdateTweets(setupList);
 
             mockContext.Verify(m => m.SaveChangesAsync(), Times.AtMost(2));
+        }
+
+        [TestMethod]
+        public async Task GetCurrentTweetsTest()
+        {
+
+            var data = setupList.AsQueryable();
+
+            var mockSet = new Mock<DbSet<Tweet>>();
+            mockSet.As<IDbAsyncEnumerable<Tweet>>().Setup(m => m.GetAsyncEnumerator()).Returns(new TestDbAsyncEnumerator<Tweet>(data.GetEnumerator()));
+
+            mockSet.As<IQueryable<Tweet>>().Setup(m => m.Provider).Returns(new TestDbAsyncQueryProvider<Tweet>(data.Provider));
+
+            mockSet.As<IQueryable<Tweet>>().Setup(m => m.Expression).Returns(data.Expression);
+            mockSet.As<IQueryable<Tweet>>().Setup(m => m.ElementType).Returns(data.ElementType);
+            mockSet.As<IQueryable<Tweet>>().Setup(m => m.GetEnumerator()).Returns(data.GetEnumerator());
+
+            var mockContext = new Mock<FullDbContext>();
+            mockContext.Setup(c => c.Set<Tweet>()).Returns(mockSet.Object);
+
+            var service = new SqliteTweetDataService(mockContext.Object);
+            var tweets = await service.GetCurrentTweets();
+
+            Assert.AreEqual(1, tweets.Count);
         }
     }
 }
